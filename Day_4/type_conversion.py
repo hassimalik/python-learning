47499b4bd8a0b1aaefa52aadb17d54bf4db35dea
Hassaan Aslam <hassaanaslam0321@gmail.com>
2026-09-26 01:41:32 +0500

    type-conversion plus its exercise

---
+++ Day_4/type_conversion.py
@@ -0,0 +1,21 @@
+age = "21"
+
+age = int(age)
+
+int("25")
+float("25.5")
+str(25)
+bool(1)
+
+
+
+price = "499.99"
+quantity = "3"
+price = float(price)
+quantity = int(quantity)
+
+total = price * quantity
+print(total)
+
+
+
