8ca7495543679a6b61c1dee845e54bf21d42f5b9
Hassaan Aslam <hassaanaslam0321@gmail.com>
2026-09-27 01:36:00 +0500

    Add initial list examples for fruits and skills

---
+++ Day_6/lists.py
@@ -0,0 +1,24 @@
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)
+
+data = ["Hassaan", 21, True, 99.5]
+
+
+print(fruits[1])
+
+
+fruits[1] = "orange"
+print(fruits)
+
+
+print(len(fruits))
+
+# Exercise
+
+skills = ["Python", "JavaScript", "React", "Next.js"]
+
+print(skills[0])
+print(skills[3])
+print(skills[0:3])
+print(len(skills))
