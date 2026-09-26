19b911349473e196c1fbc3d8d167e63fe8662c32
Hassaan Aslam <hassaanaslam0321@gmail.com>
2026-09-26 23:12:24 +0500

    Refactor string manipulation examples for clarity and consistency

--- Day_5/advanced_string_functions.py
+++ Day_5/advanced_string_functions.py
@@ -10,3 +10,19 @@ print(text.title())
 text = "   Hello Python   "
 
 print(text.strip())
+
+
+text = "I like JavaScript"
+
+print(text.replace("JavaScript", "Python"))
+
+
+text = "I am learning Python"
+
+print(text.find("Python"))
+
+print("Python" in text)
+
+skills = "Python,JavaScript,React"
+
+print(skills.split(","))
