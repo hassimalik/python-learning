b99d39a56a1979bd0c58105d194561eff523c9a1
Hassaan Aslam <hassaanaslam0321@gmail.com>
2026-09-26 22:48:10 +0500

    Add if-else examples for age categorization and user input handling

---
+++ Day_5/if-else.py
@@ -0,0 +1,21 @@
+age = 21
+
+if age >= 18:
+    print("You are an adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("You are not an adult.")
+
+
+
+age = 21
+
+if age >= 30:
+    print("You are in your thirties")
+
+elif age >= 20:
+    print("You are in your twenties")
+
+else:
+    print("You are under 20")
---
+++ Day_5/input.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+age = input("Enter your age: ")
+
+print(type(age))
+
+
+age = int(input("Enter your age: "))
+print(age + 1)
